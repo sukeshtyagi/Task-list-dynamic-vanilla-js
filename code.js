3731f981ab26308c36ccf7b2118029c31060e967
sukesh <sukeshtyagi05@gmail.com>
2024-05-31 08:20:57 +0530

    task editing functionality added

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 let taskArray = [];
+let editIndex = null;
 
 function start() {
   const inputEl = document.getElementById("inputBox");
@@ -7,22 +8,15 @@ function start() {
 start();
 
 function getInput(e) {
-  let initialInputValue = document.getElementById("inputBox").value;
-  console.log(initialInputValue);
-  let initialInputValueIndex = taskArray.indexOf(initialInputValue);
-  if (initialInputValueIndex) {
-    if (e.key === "Enter") {
-      taskArray[initialInputValueIndex] =
-        document.getElementById("inputBox").value;
-      displayTasks(taskArray);
-    }
-  } else {
-    if (e.key === "Enter") {
-      let inputValue = document.getElementById("inputBox").value;
+  if (e.key === "Enter") {
+    let inputValue = document.getElementById("inputBox").value;
+    if (editIndex !== null) {
+      taskArray[editIndex] = inputValue;
+    } else {
       taskArray.push(inputValue);
-      document.getElementById("inputBox").value = "";
-      displayTasks(taskArray);
     }
+    document.getElementById("inputBox").value = "";
+    displayTasks(taskArray);
   }
 }
 
@@ -31,55 +25,30 @@ function displayTasks(taskArray) {
   let taskDiv = document.getElementById("container");
   if (taskDiv) {
     taskDiv.remove();
-    const outerDiv = document.createElement("div");
-    outerDiv.setAttribute("id", "container");
-    for (let task of taskArray) {
-      let taskDetail = document.createElement("p");
-      const btnDiv = document.createElement("div");
-      btnDiv.style.display = "flex";
-      btnDiv.style.gap = "15px";
-      let button = document.createElement("button");
-      button.innerText = "Delete";
-      let editButton = document.createElement("button");
-      editButton.innerText = "Edit";
-      button.setAttribute("id", taskArray.indexOf(task));
-      editButton.setAttribute("id", taskArray.indexOf(task));
-      taskDetail.innerText = task;
-      outerDiv.appendChild(taskDetail);
-      outerDiv.appendChild(btnDiv);
-      btnDiv.appendChild(editButton);
-      btnDiv.appendChild(button);
+  }
+  const outerDiv = document.createElement("div");
+  outerDiv.setAttribute("id", "container");
+  for (let task of taskArray) {
+    let taskDetail = document.createElement("p");
+    const btnDiv = document.createElement("div");
+    btnDiv.style.display = "flex";
+    btnDiv.style.gap = "15px";
+    let button = document.createElement("button");
+    button.innerText = "Delete";
+    let editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    button.setAttribute("id", taskArray.indexOf(task));
+    editButton.setAttribute("id", taskArray.indexOf(task));
+    taskDetail.innerText = task;
+    outerDiv.appendChild(taskDetail);
+    outerDiv.appendChild(btnDiv);
+    btnDiv.appendChild(editButton);
+    btnDiv.appendChild(button);
 
-      button.onclick = deleteTask;
-      editButton.onclick = editTask;
+    button.onclick = deleteTask;
+    editButton.onclick = editTask;
 
-      document.body.appendChild(outerDiv);
-    }
-  } else {
-    const outerDiv = document.createElement("div");
-    outerDiv.setAttribute("id", "container");
-    for (let task of taskArray) {
-      let taskDetail = document.createElement("p");
-      const btnDiv = document.createElement("div");
-      btnDiv.style.display = "flex";
-      btnDiv.style.gap = "15px";
-      let button = document.createElement("button");
-      button.innerText = "Delete";
-      let editButton = document.createElement("button");
-      editButton.innerText = "Edit";
-      button.setAttribute("id", taskArray.indexOf(task));
-      editButton.setAttribute("id", taskArray.indexOf(task));
-      taskDetail.innerText = task;
-      outerDiv.appendChild(taskDetail);
-      outerDiv.appendChild(btnDiv);
-      btnDiv.appendChild(editButton);
-      btnDiv.appendChild(button);
-
-      button.onclick = deleteTask;
-      editButton.onclick = editTask;
-
-      document.body.appendChild(outerDiv);
-    }
+    document.body.appendChild(outerDiv);
   }
 }
 
@@ -93,9 +62,6 @@ function deleteTask(e) {
 function editTask(e) {
   console.log("eidt");
   console.log(taskArray[e.target.id]);
+  editIndex = e.target.id;
   document.getElementById("inputBox").value = taskArray[e.target.id];
-  if (e.key === "Enter") {
-    console.log("object");
-  }
-  //taskArray[taskArray[e.target.id]] = displayTasks(taskArray);
 }
